822eb8aa0c03e7d51ba998e40b607e4315a1c4ab
Abdullah Alansary <abdullahalansary2010@gmail.com>
2023-12-04 20:20:27 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  // State for the person's information
+  const [person, setPerson] = useState({
+    fullName: 'John Doe',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    imgSrc: 'https://placekitten.com/200/200', // Replace with the actual image source
+    profession: 'Software Developer',
+  });
+
+  // State to control whether to show the person's profile
+  const [show, setShow] = useState(false);
+
+  // State to keep track of the time interval since the component mounted
+  const [interval, setInterval] = useState(0);
+
+  // useEffect to handle the interval setup and cleanup
+  useEffect(() => {
+    // Setup interval to update the time every second
+    const intervalId = setInterval(() => {
+      setInterval((prevInterval) => prevInterval + 1);
+    }, 1000);
+
+    // Cleanup function to clear the interval when the component is unmounted
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []); // Empty dependency array to run the effect only once on mount
+
+  // Function to toggle the visibility of the person's profile
+  const toggleShow = () => {
+    setShow((prevShow) => !prevShow);
+  };
+
+  // JSX to render the component
+  return (
+    <div className="App">
+      {/* Button to toggle the profile visibility */}
+      <button onClick={toggleShow}>Toggle Profile</button>
+      
+      {/* Conditionally render the person's profile if 'show' is true */}
+      {show && (
+        <div>
+          <h1>{person.fullName}</h1>
+          <p>{person.bio}</p>
+          <img src={person.imgSrc} alt="Profile" />
+          <p>Profession: {person.profession}</p>
+        </div>
+      )}
+      
+      {/* Display the time since mount */}
+      <p>Time since mount: {interval} seconds</p>
+    </div>
+  );
+};
+
+export default App;
